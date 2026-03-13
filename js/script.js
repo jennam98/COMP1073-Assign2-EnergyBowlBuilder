fbcd8f096794315e067e232e8e53949c9f7ac363
TeresaH16 <heredia.t@icloud.com>
2026-03-13 14:50:08 -0400

    class EnergyBowl created.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+class EnergyBowl {
+    constructor(name, email, phone, size, base, protein, toppings, cheese, sauce, specialInstructions) {
+        this.orderNumber = this.generateOrderNumber();
+    }
+}
