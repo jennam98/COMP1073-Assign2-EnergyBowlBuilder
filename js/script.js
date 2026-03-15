b7f3ce58034635d0d30c66f609dab6236483d239
TeresaH16 <heredia.t@icloud.com>
2026-03-15 17:25:21 -0400

    Merge branch 'Main' into Teresa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 class EnergyBowl {
     constructor(name, email, phone, size, base, protein, toppings, cheese, sauce, specialInstructions) {
         this.orderNumber = this.generateOrderNumber();
@@ -9,55 +8,24 @@ class EnergyBowl {
         this.base = base;
         this.protein = protein;
         this.toppings = toppings;
-        this.cheese = cheese;
+        this.chesse = cheese;
         this.sauce = sauce;
         this.specialInstructions = specialInstructions;
     }
 
-    
-}
-
-
-document.getElementById("energyBowlForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    // Collect form data
-
-    //User info
-
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-
-    //Bowl Info
-
-    const size = document.getElementById("size").value;
-    const base = document.getElementById("base").value;
-    const protein = document.getElementById("protein").value;
-
-    const toppingCheckboxes = document.querySelectorAll('input[name="toppings"]:checked');
-    const toppings = [];
-
-
-    const cheese = document.getElementById("cheese").value;
-    const sauce = document.getElementById("sauce").value;
-    const specialInstructions = document.getElementById("specialInstructions").value.trim();
-
-    toppingCheckboxes.forEach(function(topping) {
-        toppings.push(topping.value);
-
-
-    //Error message 
-    const errorMessage = document.getElementById("errorMessage");
-    errorMessage.textContent = "";
-
-    });
-
-
-    // Validate form data
-    if (!name || !email || !phone || !size || !base || !protein || !cheese || !sauce) {
-        errorMessage.textContent = "Please fill in all required fields.";
-        return;
+    generateOrderNumber() {
+        return Math.floor(10000 + Math.random() * 90000);
     }
 
-});
+    formatText(value) {
+        return value
+            .replace(/_/g, " ")
+            .replace(/\b\w/g, function (char) {
+                return char.toUpperCase();
+            });
+    }
+
+    
+
+
+}
